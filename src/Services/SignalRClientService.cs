9526c786b0b39145780d24a1404eaf521507a361
Aymen BEN MILED <benmiledaymen@hotmail.com>
2024-09-30 17:07:25 +0200

    add autoreconnect on start

--- src/Services/SignalRClientService.cs
+++ src/Services/SignalRClientService.cs
@@ -21,55 +21,78 @@ namespace ServiceHubClient.Services
         }
         public async Task<HubConnection?> WaitingForYourOrderSir()
         {
-            var hubUrl = _signalRSetting.Url;
+            HubConnection? connection = null;
 
-            // Create a connection to the SignalR hub
-            var connection = new HubConnectionBuilder()
-                .WithUrl(hubUrl, options =>
-                {
-                    options.AccessTokenProvider = () => Task.FromResult(_signalRSetting.AccessToken);
-                    // options.Headers.Add("Authorization", "Bearer your-access-token");
-                    options.Headers.Add("CI", _signalRSetting.ClientId);
-                    options.Headers.Add("T", DateTime.UtcNow.ToString("o"));
-                })
-                .WithAutomaticReconnect()
-                .Build();
-
-            // Define a handler for when the connection is closed
-            connection.Closed += async (error) =>
+            while (connection == null || connection?.State == HubConnectionState.Disconnected)
             {
-                _logger.LogInformation("Connection closed. Trying to reconnect...");
-                await Task.Delay(_signalRSetting.DelayBeforeReconnect);
-                await connection.StartAsync();
-            };
+                var hubUrl = _signalRSetting.Url;
 
-            // Handle any messages from the server (this matches the method in the hub)
-            connection.On<string>("Execute", async (message) =>
-            {
-                _logger.LogInformation($"Received message: {message}");
+                // Create a connection to the SignalR hub
+                connection = new HubConnectionBuilder()
+                    .WithUrl(hubUrl, options =>
+                    {
+                        options.AccessTokenProvider = () => Task.FromResult(_signalRSetting.AccessToken);
+                        // options.Headers.Add("Authorization", "Bearer your-access-token");
+                        options.Headers.Add("CI", _signalRSetting.ClientId);
+                        options.Headers.Add("T", DateTime.UtcNow.ToString("o"));
+                    })
+                    .WithAutomaticReconnect()
+                    .Build();
 
-                if (message == "synchronize")
+                // Define a handler for when the connection is closed
+                connection.Closed += async (error) =>
                 {
-                    await _localAgentService.Synchronize();
+                    bool connectionSucceded = false;
+                    while (connectionSucceded == false)
+                    {
+                        try
+                        {
+                            _logger.LogInformation("Connection closed. Trying to reconnect...");
+                            await Task.Delay(_signalRSetting.DelayBeforeReconnect);
+                            await connection.StartAsync();
+                            _logger.LogInformation("RecConnected to the SignalR hub.");
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogInformation($"Connection error: {ex.Message}");
+                        }
+                    }
+                };
+
+                // Handle any messages from the server (this matches the method in the hub)
+                connection.On<string>("Execute", async (message) =>
+                {
+                    _logger.LogInformation($"Received message: {message}");
+
+                    if (message == "synchronize")
+                    {
+                        await _localAgentService.Synchronize();
+                    }
+
+                    if (message == "export")
+                    {
+                        await _localAgentService.Export();
+                    }
+                });
+
+                try
+                {
+                    // Start the connection to the SignalR hub
+                    await connection.StartAsync();
+                    _logger.LogInformation("Connected to the SignalR hub.");
+
+                    _logger.LogInformation("Waiting for message...");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogInformation($"Connection error: {ex.Message}");
                 }
 
-                if (message == "export")
+                if (connection == null || connection?.State == HubConnectionState.Disconnected)
                 {
-                    await _localAgentService.Export();
+                    await Task.Delay(_signalRSetting.DelayBeforeReconnect);
+                    _logger.LogInformation("Connection not started Trying to reconnect...");
                 }
-            });
-
-            try
-            {
-                // Start the connection to the SignalR hub
-                await connection.StartAsync();
-                _logger.LogInformation("Connected to the SignalR hub.");
-
-                _logger.LogInformation("Waiting for message...");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogInformation($"Connection error: {ex.Message}");
             }
 
             return connection;
